b65e3d6ed78f628f1a2c436dcbcf45b8a1596420
Vaibhav R. Kumar <vaibhavk667@gmail.com>
2018-06-10 17:54:24 -0700

    June 10, 2018
    -Wroked on scene anim (animAppear)

---
+++ Spellcraft - COSC Summer Project/Assets/Scripts/animAppear.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class animAppear : MonoBehaviour {
+
+    public float animationTime = 0.01f;
+    public Texture2D texture;
+
+    private Image image;
+    private Color panelColor = Color.black;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Time.timeSinceLevelLoad < animationTime){
+            //perform animation
+
+            float timeChange = Time.deltaTime / animationTime;
+            panelColor.a -= timeChange;
+            image.color = panelColor;
+        }
+        else{
+            gameObject.SetActive(false);
+        }
+		
+	}
+}
